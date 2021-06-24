3227010ccb692f3a23a88780163fe888f6fd429f
zhangwen <apply_count@sina.com>
2021-06-24 17:26:34 +0800

    asdf

--- ML/tf/tf1/cluster/Worker0.py
+++ ML/tf/tf1/cluster/Worker0.py
@@ -8,7 +8,7 @@ ps_hosts = ["9.134.189.246:9500"]
 cluster = tf.train.ClusterSpec({"worker": worker_hosts, "ps":ps_hosts})
 
 server=tf.train.Server(cluster,job_name='worker',task_index=0)#找到‘worker’名字下的，task0，也就是机器A
-with tf.device(tf.train.replica_device_setter(cluster=cluster)):
+with tf.device(tf.train.replica_device_setter()):
     w = tf.get_variable('w',(1),tf.float32,initializer=tf.constant_initializer(2))
     add = tf.add(w, 1)
     update = tf.assign(w, add)
