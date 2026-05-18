341556b2dd81b6ced4cff66991c1e8260cd86b29
thielzhang <thielzhang@tencent.com>
2026-05-18 09:29:03 +0800

    remove finacial

--- finacial/get_xag.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import eastmoney_api as finacial_api
-import finacial_db
-
-
-if __name__ == "__main__":
-    #finacial_db.update_all_ma("XAG")
-    finacial_db.create_stock_table(None, None, "XAU")
-    
